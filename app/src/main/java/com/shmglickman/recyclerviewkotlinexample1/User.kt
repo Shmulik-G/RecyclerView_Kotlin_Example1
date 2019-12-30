364eda09288e3405ce8435dad57e936b121e3719
Shmulik-G <Glickmanso@gmail.com>
2019-12-31 00:07:22 +0200

    Initial commit V1

---
+++ app/src/main/java/com/shmglickman/recyclerviewkotlinexample1/User.kt
@@ -0,0 +1,11 @@
+package com.shmglickman.recyclerviewkotlinexample1
+
+/**
+ * Created by:   Shmulik Glickman on 30/12/2019, 19:04.
+ * Package Name: com.shmglickman.recyclerviewkotlinexample1.
+ */
+
+data class User(
+    val name: String,
+    val address: String
+)
